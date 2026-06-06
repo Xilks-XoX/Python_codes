4d917d9a5ee9bff6fa2b78bf831ed7cb74ea4680
Xilks-XoX <appblecore12@gmail.com>
2026-06-06 10:27:30 +0700

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,30 @@
+# num1 = int(input("number:"))
+# num2 = int(input("number:"))
+# op = input("method:")
+# def add():
+#     print(num1 + num2)
+# def sub():
+#     print(num1 - num2)
+# def mul():
+#     print(num1 * num2)
+# def div():
+#     print(num1 / num2)
+# def mod():
+#     print(num1 % num2)
+# def pow():
+#     print(num1 ** num2)
+# if op == "add":
+#     add()
+# elif op == "subtract":
+#     sub()
+# elif op == "mulitiply":
+#     mul()
+# elif op == "divide":
+#     div()
+# elif op == "module":
+#     mod()
+# elif op == "power":
+#     pow()
+# else:
+#     print("error")
+
