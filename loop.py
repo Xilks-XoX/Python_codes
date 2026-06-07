62a685367e9b1ac78eb100013f0f434408a3efa4
Xilks <appblecore12@gmail.com>
2026-06-07 12:21:12 +0700

    loop.py

---
+++ loop.py
@@ -0,0 +1,38 @@
+# 0
+# while True:
+#     import random
+#     num1 = random.randint(1, 100)
+#     num2 = int(input("number:"))
+#     if num1 == num2:
+#         print("Congrats")
+#         break
+
+# 1
+# for j in range (1, 11):
+#     print(j)
+
+# 2
+# word = input("word:")
+# for word in (word):
+#     print(word)
+
+# 3
+# for countdown in range (5, 0, -1):
+#     print(countdown)
+# print("Blast off!")
+
+# 4
+# num1 = 0
+# while True:
+#     num = int(input("number:"))
+#     num1 += num
+#     print(num1)
+#     if num1 >= 50:
+#         break
+#     else:
+#         continue
+   
+# 5
+# num = int(input("number:"))
+# for int in range (1, 13):
+#     print(num, "*", int, "=", num*int)
