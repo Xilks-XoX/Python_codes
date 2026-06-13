2e51fa3e87cb65d4ae0eeb13aed5a6f4b74b0435
Xilks <appblecore12@gmail.com>
2026-06-13 09:52:39 +0700

    list.py

---
+++ list.py
@@ -0,0 +1,9 @@
+# guests = ["Alice", "Bob", "Charlie"]
+# guests.insert(0, "Daisy")
+# guests.append("Ethan")
+# guests.extend(["Fiona", "George"])
+# guests.remove("Bob")
+# last_guest = guests.pop()
+# for people in guests:
+#     print("-", people)
+# print("last guest:", last_guest)
