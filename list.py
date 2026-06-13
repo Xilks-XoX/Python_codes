c8f4bb51285116547361aed17f1a3a314497f4b5
Xilks <appblecore12@gmail.com>
2026-06-13 10:08:47 +0700

    list.py

--- list.py
+++ list.py
@@ -7,3 +7,13 @@
 # for people in guests:
 #     print("-", people)
 # print("last guest:", last_guest)
+
+# playlist = ["Song A", "Song B", "Song C"]
+# playlist.insert(0, "Intro Track")
+# playlist.append("Bonus Track")
+# playlist.extend(["Song D", "Song E"])
+# playlist.remove("Song B")
+# removed_track = playlist.pop(0)
+# for songs in playlist:
+#     print(songs)
+# print("removed track:", removed_track)
