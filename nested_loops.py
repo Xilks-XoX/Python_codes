72f46452b5c1e945b567755cb630d66c770df99c
Xilks <appblecore12@gmail.com>
2026-06-28 10:29:10 +0700

    nested_loops.py

---
+++ nested_loops.py
@@ -0,0 +1,56 @@
+# main = ["Burger", "Pizza", "Salad"]
+# side = ["Fries", "Soup"]
+# for i in main:
+#     for j in side:
+#         print(i, "with", j)
+
+# x = [1, 2]
+# y = [1, 2, 3]
+# for a in x:
+#     for b in y:
+#         print("x:", a, "y:", b)
+
+# attempts = 1
+# while attempts <= 3:
+#     step = 1
+#     while step <= 2:
+#         print(f"Attempt {attempts}: Checking Step {step}...")
+#         step += 1
+#     print(f"Attempt {attempts} failed. Moving to next attempt.")
+#     attempts += 1
+
+# sec = 3
+# while sec > 0:
+#     checks = 1
+#     while checks <= 2:
+#         print(f"T-minus {sec} seconds: Running System Check {checks}")
+#         checks += 1
+#     sec -= 1
+# print("Blast off")
+
+# batteries = ["Battery A", "Battery B"]
+# for i in batteries:
+#     charge = 70
+#     while charge < 100:
+#         print(f"{i} is at {charge}% charging...")
+#         charge += 10
+#     print(f"{i} is fully charged ({charge}%)")
+
+# lvls = ["Stereeo Madness", "Back On Track", "Polargeist"]
+# dwg = 0
+# for i in lvls:
+#     print(f"--- Starting Practice: {i} ---")
+#     cp = 40
+#     while cp < 100:
+#         print(f"Playing {i}... Current Best: {cp}%")
+#         cp += 20
+#         dwg += 1
+# print(f"Total days spent grinding without touching grass: {dwg}")
+
+# days = 1
+# exercises = ["Pushups", "Sqauts"]
+# while days <= 3:
+#     print(f"--- Day {days} Start ---")
+#     for tasks in exercises:
+#         print(f"Doing: {tasks}")
+#     days += 1
