7cfdc09d72fe14a0c7ef2c1ecc8d79f5bb638a87
Xilks <appblecore12@gmail.com>
2026-07-04 10:30:36 +0700

    class.py

--- class.py
+++ class.py
@@ -11,3 +11,16 @@
 # c = room("4A", 100, "Bright")
 # c.preview()
 # c.teacher()
+
+# class circle:
+#     def __init__ (self, r):
+#         self.r = r
+#     def get_area(self):
+#         A = 3.142 * (self.r**2)
+#         print(f"Area: {A}")
+#     def get_circumference(self):
+#         C = 2 * 3.142 * (self.r)
+#         print(f"Circumference: {C}")
+# radius = circle(7)
+# radius.get_area()
+# radius.get_circumference()
