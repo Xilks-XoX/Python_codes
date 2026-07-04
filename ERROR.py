352261073be28d56460772abb3c78d97eb64e6ed
Xilks <appblecore12@gmail.com>
2026-07-04 10:06:59 +0700

    ERROR.py

--- ERROR.py
+++ ERROR.py
@@ -52,24 +52,3 @@
 # num = [1, 2, 3, 4, 5, 6]
 # for i in range(1, 6):
 #     print(random.choice(num))
-
-# words_list = ["append", "input", "tie", "win"]
-# def filter_long_word(words_list):
-#     new_word = input("word:")
-#     words_list.append(new_word)
-#     for i in words_list:
-#         if len(i) > 4:
-#             print(f"-{i}")
-# filter_long_word(words_list)# class room:
-#     def __init__(self, classname, students, teacher_name):
-#         self.classname = classname
-#         self.students = students
-#         self.teacher_name = teacher_name
-#     def preview(self):
-#         print(f"class: {self.classname}")
-#         print(f"students: {self.students}")
-#     def teacher(self):
-#         print(f"teacher: {self.teacher_name}")
-# c = room("4A", 100, "Bright")
-# c.preview()
-# c.teacher()
