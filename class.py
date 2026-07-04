cbb0a57baa0998f4a06ec16b3eead3ebd25e7426
Xilks <appblecore12@gmail.com>
2026-07-04 10:07:33 +0700

    class.py

---
+++ class.py
@@ -0,0 +1,20 @@
+# words_list = ["append", "input", "tie", "win"]
+# def filter_long_word(words_list):
+#     new_word = input("word:")
+#     words_list.append(new_word)
+#     for i in words_list:
+#         if len(i) > 4:
+#             print(f"-{i}")
+# filter_long_word(words_list)# class room:
+#     def __init__(self, classname, students, teacher_name):
+#         self.classname = classname
+#         self.students = students
+#         self.teacher_name = teacher_name
+#     def preview(self):
+#         print(f"class: {self.classname}")
+#         print(f"students: {self.students}")
+#     def teacher(self):
+#         print(f"teacher: {self.teacher_name}")
+# c = room("4A", 100, "Bright")
+# c.preview()
+# c.teacher()
