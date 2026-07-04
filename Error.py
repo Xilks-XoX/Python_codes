0d0a2515ad0d83490348e9c8bfc230e0646873ec
Xilks <appblecore12@gmail.com>
2026-07-04 10:04:02 +0700

    Error.py

--- Error.py
+++ Error.py
@@ -17,7 +17,7 @@
 # try:
 #     "2" + 24
 # except TypeError:
-#     print("son 😭😭😭😭")
+#     print("son)
 
 # try:
 #     num1 = input("number:")
@@ -29,3 +29,47 @@
 #     print(f"heres your number {int1} and {int2}")
 # except TypeError as e:
 #     print(f"Error: {e}")
+
+# import random
+# def guess_number():
+#     while True:
+#         try:
+#             num  = random.randint(1, 10)
+#             guess = int(input("number:"))
+#             if num ==  guess:
+#                 print("correct")
+#                 break
+#             else:
+#                 print(f"correct number: {num}")
+#                 print("")
+#                 continue
+#         except ValueError:
+#             print("NUMBERS")
+#             print("")
+# guess_number()
+
+# import random
+# num = [1, 2, 3, 4, 5, 6]
+# for i in range(1, 6):
+#     print(random.choice(num))
+
+# words_list = ["append", "input", "tie", "win"]
+# def filter_long_word(words_list):
+#     new_word = input("word:")
+#     words_list.append(new_word)
+#     for i in words_list:
+#         if len(i) > 4:
+#             print(f"-{i}")
+# filter_long_word(words_list)# class room:
+#     def __init__(self, classname, students, teacher_name):
+#         self.classname = classname
+#         self.students = students
+#         self.teacher_name = teacher_name
+#     def preview(self):
+#         print(f"class: {self.classname}")
+#         print(f"students: {self.students}")
+#     def teacher(self):
+#         print(f"teacher: {self.teacher_name}")
+# c = room("4A", 100, "Bright")
+# c.preview()
+# c.teacher()
