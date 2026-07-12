4aa02a0befe2dc8ba70af6c80f0399bd7ffb3256
Xilks <appblecore12@gmail.com>
2026-07-12 10:27:22 +0700

    class.py

--- class.py
+++ class.py
@@ -24,3 +24,60 @@
 # radius = circle(7)
 # radius.get_area()
 # radius.get_circumference()
+
+# class BankAccount:
+#     def __init__(self, account_holder, balance):
+#         self.account_holder = account_holder
+#         self.balance = balance
+#     def deposit(self, amount):
+#         try:
+#             if amount <= 0:
+#                 raise ValueError("deposit: Amount must be positive")
+#             self.balance += amount
+#         except ValueError as e:
+#             print(f"\n[ERROR] {e}")
+#         except TypeError:
+#             print("\n[ERROR] deposit: Invalid number type")
+#     def withdraw(self, amount):
+#         try:
+#             if amount <= 0:
+#                 raise ValueError("withdraw: Amount must be positive")
+#             elif amount > self.balance:
+#                 raise ValueError("withdraw: Insufficient funds")
+#             self.balance -= amount      
+#         except ValueError as e:
+#             print(f"[ERROR] {e}")
+#         except TypeError:
+#             print("[ERROR] withdraw: Invalid number type")
+#     def display_balance(self):
+#         print("\n--- Account Status ---")
+#         print(f"Account Holder: {self.account_holder}")
+#         print(f"Balance: ${self.balance:.2f}")
+#         print("----------------------\n")
+# print("Welcome to the World Government Bank")
+# BA = BankAccount("Xilks", 1000000)
+# print("What do you want to do today?")
+# while True:
+#     print("1. Deposit")
+#     print("2. Withdraw")
+#     print("0. Cancel")
+#     choice = input("Enter choice:").strip()
+#     if choice == "1":
+#         try:
+#             d = float(input("Enter deposit amount: "))
+#             BA.deposit(d)
+#             BA.display_balance()
+#         except ValueError:
+#             print("[ERROR] Please enter a valid number for deposit.\n")        
+#     elif choice == "2":
+#         try:
+#             w = float(input("Enter withdraw amount: "))
+#             BA.withdraw(w)
+#             BA.display_balance()
+#         except ValueError:
+#             print("[ERROR] Please enter a valid number for withdrawal.\n")         
+#     elif choice == "0":
+#         break        
+#     else:
+#         print("[ERROR] Invalid choice. Please select 1, 2, or 0.\n")
+# print("\nThank you for using our service")
