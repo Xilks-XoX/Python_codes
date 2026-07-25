44fe7e68f25dbaac6dc5e1da7dae79b5717755b4
Xilks <appblecore12@gmail.com>
2026-07-25 10:31:13 +0700

    dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+# Inventory = {"apples": 3,
+#              "potatoes": 12, 
+#              "carrots": 8}
+# print(f"potatoes: {Inventory["potatoes"]}")
+# Inventory["bananas"] = 3
+# Inventory["apples"] += 3
+# print(Inventory)
+
+# translator = {"hello" : "hola",
+#              "cat" : "geto",
+#              "water" : "agua"}
+# word = input("word:")
+# if word in translator:
+#     print(f"translation: {translator[word]}")
