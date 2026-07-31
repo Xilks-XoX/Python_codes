d0a22a1d2bc3b59c0f1fd6bec25b4411ad25e40a
Xilks <appblecore12@gmail.com>
2026-07-31 18:45:11 +0700

    dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -12,3 +12,34 @@
 # word = input("word:")
 # if word in translator:
 #     print(f"translation: {translator[word]}")
+
+# class Pet:
+    # def __init__(self, pet_name, species):
+        # self.pet_name = pet_name
+        # self.species = species
+        # self.health_checkup = { 
+            # "weight_kg": 10,
+           # "is_vaccinated": False
+       # }
+    # def update_health(self, amount):
+       #  self.health_checkup["weight_kg"] += amount
+        # print(self.health_checkup)
+# p = Pet("Guy", "Saber-toothed cat")
+# amount = int(input("amount: "))
+# p.update_health(amount)
+
+# class Toy:
+    # def __init__(self, name, price):
+        # self.name = name
+        # self.price = price
+# toy1 = Toy("Plushie", 1000)
+# toy2 = Toy("LEGO", 10000)
+# toy3 = Toy("Puppet", 100)
+# store_catalog = {
+    # toy1.name : toy1,
+    # toy2.name : toy2,
+    # toy3.name : toy3
+# }
+# chosen_toy = store_catalog["LEGO"]
+# print(f"Toy name: {chosen_toy.name}")
+# print(f"Toy price: {chosen_toy.price}")
