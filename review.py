110c5039699d6b18e23cbeb9757ea1a156945fc8
Xilks <appblecore12@gmail.com>
2026-08-08 10:20:58 +0700

    review.py

---
+++ review.py
@@ -0,0 +1,25 @@
+# from turtle import *
+# traffic = ["red", "yellow", "green"]
+# light = input("Light color:")
+# l_light = light.lower()
+# if l_light in traffic:
+#     if l_light == "red":
+#         forward(0)
+#     elif l_light == "yellow":
+#         forward(50)
+#     elif l_light == "green":
+#         forward(100)
+#     print(f"Uppercase: {light.upper()}")
+# else:
+#     print("[ERROR] invalid light color")
+
+# players = {
+#     "SpokeIsHere" : 20,
+#     "ParrotX2" : 14,
+#     "Max" : 10,
+#     "FlameFrags" : 20
+# }
+# for i in players:
+#     print(f"Name: {i}, Score: {players[i]}")
+# high_scorers = {name: score for name, score in players.items() if score > 15}
+# print(high_scorers)
